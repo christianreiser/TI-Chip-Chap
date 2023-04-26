6501fe0095074cdb4ab29da20f95a1e065e83f32
christian-reiser <cre@e-dialog.group>
2023-04-26 17:30:50 +0200

    tried turbo model

--- chatbot_service/src/indexing/indexing.py
+++ chatbot_service/src/indexing/indexing.py
@@ -1,12 +1,19 @@
 import os
 
-from gpt_index import SimpleDirectoryReader, GPTSimpleVectorIndex
-
+from langchain.chat_models import ChatOpenAI
+from llama_index import (
+    LLMPredictor,
+    ServiceContext,
+    SimpleDirectoryReader, GPTSimpleVectorIndex
+)
 
 def construct_index(directory_path):
     print('constructing index...')
     documents = SimpleDirectoryReader(directory_path).load_data()
-    index = GPTSimpleVectorIndex.from_documents(documents)
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo"))
+    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
+
+    index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
     index.save_to_disk('index.json')
     print('index constructed and saved to disk at index.json')
     return index
--- chatbot_service/src/main.py
+++ chatbot_service/src/main.py
@@ -1,11 +1,17 @@
 import gradio as gr
 import os
-from gpt_index import GPTSimpleVectorIndex
 from google.cloud import storage
 import json
 from datetime import datetime
-
-os.environ["OPENAI_API_KEY"] = os.getenv("openai_api_key")
+from langchain import OpenAI
+from langchain.chat_models import ChatOpenAI
+from llama_index import (
+    LLMPredictor,
+    ServiceContext,
+    GPTSimpleVectorIndex
+)
+# os.environ["OPENAI_API_KEY"] = os.getenv("openai_api_key")
+os.environ["OPENAI_API_KEY"] = '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'
 
 
 def read_json_from_bucket_with_cache(bucket_name, file_name):
@@ -22,25 +28,33 @@ def read_json_from_bucket_with_cache(bucket_name, file_name):
 
 def chatbot(input_text):
     global index_cache
+    global service_context
 
     if index_cache is None:
         bucket_name = os.getenv("index_bucket_name")
         file_path = 'index.json'
         json_data = read_json_from_bucket_with_cache(bucket_name, file_path)
         index_cache = GPTSimpleVectorIndex.load_from_dict(json_data)
-
-    response = index_cache.query(input_text, response_mode="compact")
+    # index_cache = GPTSimpleVectorIndex.load_from_disk('indexing/index.json')
+    # llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0, max_tokens=900, model_name="gpt-3.5-turbo"))
+    if service_context is None:
+        llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, max_tokens=900, model_name="text-davinci-003"))
+        service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, chunk_size_limit=900)
+    response = index_cache.query(response_mode="compact", query_str=input_text,service_context=service_context)
     return response.response
 
 
-index_cache = None
-json_data_cache = None
+index_cache, service_context, json_data_cache = None, None, None
+
 iface = gr.Interface(fn=chatbot,
                      inputs=gr.components.Textbox(lines=7, label="Enter your text about Chris"),
                      outputs="text",
-                     title="Chris chatbot. last updated:" + str(datetime.now()),
+                     title="Chris chatbot",
+                     description="Last updated:" + str(datetime.utcnow()) + " (UTC)",
                      flagging_options=["bad answer", "incorrect", "good answer", "not relevant"],
-                     examples=['welche blutwerte  hatte Christian am 2019-09-05? schreibe eine zeile pro laborident. ', 'wie ist die Persönlichkeitsstrukturvon christian?', 'wo arbeitet christian derzeit?'],
+                     examples=['welche blutwerte hatte Christian am 2019-09-05? z.b.HDL,LDL,NF,QCH,GT,AP,BILI,OT,...  schreibe eine zeile pro laborident. ',
+                               'was steht im Persönlichkeitsstrukturtest PST-R von Klient Reiser, Christian?',
+                               'wo arbeitet christian derzeit?'],
                      interpretation="default",
                      theme=gr.themes.Default(primary_hue="green", secondary_hue="blue"),
                      )
