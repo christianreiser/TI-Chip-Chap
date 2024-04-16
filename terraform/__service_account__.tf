c3d4ea5ee913ecda8b626fd653800afbb9db7165
Christian Reiser <chrisi.reiser@gmail.com>
2024-04-16 20:36:55 +0200

    Update __service_account__.tf

--- terraform/__service_account__.tf
+++ terraform/__service_account__.tf
@@ -6,5 +6,5 @@ resource "google_service_account" "chatbot-service" {
 resource "google_project_iam_member" "chatbot_service_bq_viewer" {
   project = "chatbot-420"
   role    = "roles/bigquery.dataViewer"
-  member  = "serviceAccount:${google_service_account.chatbot_service.email}"
+  member  = "serviceAccount:${google_service_account.chatbot-service.email}"
 }
